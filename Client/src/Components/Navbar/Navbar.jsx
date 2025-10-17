cf72ecdc2f0f67e66ff2a04e853ac6b215759f06
Mahidharan <mahidharan5612@gmail.com>
2025-10-17 14:55:18 +0530

    nav

--- Client/src/Components/Navbar/Navbar.jsx
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/logo.jpg";
+import UserIcon from "../../assets/usericon.jpg";
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+     <nav className="navbar">
+      <div className="nav-left">
+        <img src={Logo} alt="NextStep Logo" className="logo" />
+        <h1 className="title">NextStep</h1>
+      </div>
 
-export default Navbar
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>My Profile</li>
+        <li>Chat</li>
+        <li>LogOut</li>
+      </ul>
 
+      <div className="nav-right">
+        <img src={UserIcon} alt="User" className="profile-img" />
+      </div>
+    </nav>
+  );
+};
+ 
+
+export default Navbar;
