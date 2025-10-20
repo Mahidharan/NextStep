8134fd24a79cadd9f65f06ab65b149e7fee95522
Mahidharan <mahidharan5612@gmail.com>
2025-10-20 14:04:29 +0530

    Loginpage

--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
+import Logo from "../../assets/logo.jpg";
 
 function Login() {
   return (
-    <div>
-      
+    <div className="login-container">
+      <div className="headings"></div>
+      <div className="outside-card">
+        <div className="inside-card">
+          <img src={Logo} alt="" />
+          <p>Share, Learn, and Grow Together</p>
+          <button className="login-btn"> Continue With Google</button>
+          <p className="terms">
+            By continuing, you agree to our <span>Terms of service </span> and
+            <span> Privacy Policy</span>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
