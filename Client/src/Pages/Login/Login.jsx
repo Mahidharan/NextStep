a8d8f994d5c40d75bdffe266d8a448c9dae1b80e
Mahidharan <mahidharan5612@gmail.com>
2025-12-27 21:43:09 +0530

    Login Page Upgrade

--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -15,38 +15,29 @@ function Login() {
 
   return (
     <div className="login-container">
-      <div className="headings">
-        <h2>Share, Learn, and Experience Interviews with NextStep</h2>
-        <p>
-          Connect with Peers, Share your experience, and improve your content
-          with AI
+      <div className="inside-card">
+        <img src={Logo} alt="" />
+        <p>Share, Learn, and Grow Together</p>
+        <button
+          className="login-btn"
+          onClick={() => {
+            window.location.href = `${
+              import.meta.env.VITE_BACKEND_URL
+            }/api/user/google`;
+          }}
+        >
+          <FcGoogle />
+          Continue With Google
+        </button>
+        <p className="terms">
+          By continuing, you agree to our{" "}
+          <span onClick={() => changePage("/terms")}>Terms of service </span>{" "}
+          and
+          <span onClick={() => changePage("/privacy-policy")}>
+            {" "}
+            Privacy Policy
+          </span>
         </p>
-      </div>
-      <div className="outside-card">
-        <div className="inside-card">
-          <img src={Logo} alt="" />
-          <p>Share, Learn, and Grow Together</p>
-          <button
-            className="login-btn"
-            onClick={() => {
-              window.location.href = `${
-                import.meta.env.VITE_BACKEND_URL
-              }/api/user/google`;
-            }}
-          >
-            <FcGoogle />
-            Continue With Google
-          </button>
-          <p className="terms">
-            By continuing, you agree to our{" "}
-            <span onClick={() => changePage("/terms")}>Terms of service </span>{" "}
-            and
-            <span onClick={() => changePage("/privacy-policy")}>
-              {" "}
-              Privacy Policy
-            </span>
-          </p>
-        </div>
       </div>
     </div>
   );
